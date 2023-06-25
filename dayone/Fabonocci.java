f3b31de8ac47e1ab4f36d8a881cdba3eda92837f
Suresh-08 <137680326+Suresh-08@users.noreply.github.com>
2023-06-25 13:58:04 +0530

    Fabonocci.java

--- dayone/Fabonocci.java
+++ dayone/Fabonocci.java
@@ -1,10 +1,26 @@
 package week1.dayone;
 
+import java.util.Iterator;
+
 public class Fabonocci {
 
 	public static void main(String[] args) {
 		
+int n = 15;
 
+int firstnumber = 0;
+ int secondnumber = 1;
+ 
+ System.out.println("print fabonacci series");
+ 
+ for (int i = 1; i<=n;i++) {
+	 
+	 int sum = firstnumber + secondnumber;
+	 firstnumber = secondnumber;
+	 secondnumber = sum;
+	 System.out.println(firstnumber + " ");
+ }
+ 
 	}
 
 }
